cc4ad007801669b87f2d2c72eec5189dfb6cf3c4
Christian Palmstierna <christian.palmstierna@gmail.com>
2018-08-21 11:50:34 +0200

    Initial commit

---
+++ Cpx86.Swashbuckle.AspNetCore.Discriminator/Extensions/SwaggerGeneratorOptionsExtensions.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Cpx86.Swashbuckle.AspNetCore.Discriminator.Extensions
+{
+    public static class SwaggerGeneratorOptionsExtensions
+    {
+        public static void Polymorphic(this SwaggerGenOptions options, Action<SwaggerPolymorphicOptions> builder)
+        {
+            var polyOptions = new SwaggerPolymorphicOptions();
+            builder(polyOptions);
+        }
+    }
+
+    public class SwaggerPolymorphicOptions
+    {
+        public PolymorphicTypeBuilder<T> Add<T>()
+        {
+            var polymorphicTypeBuilder = new PolymorphicTypeBuilder<T>();
+            TypeBuilders.Add(polymorphicTypeBuilder);
+            return polymorphicTypeBuilder;
+        }
+
+        internal List<PolymorphicTypeBuilder> TypeBuilders { get; } = new List<PolymorphicTypeBuilder>();
+    }
+
+    public abstract class PolymorphicTypeBuilder
+    {
+        internal List<DiscriminatorBuilder> DiscriminatorBuilders { get; } = new List<DiscriminatorBuilder>();
+        internal abstract Type Type { get; }
+    }
+
+    public class PolymorphicTypeBuilder<T> : PolymorphicTypeBuilder
+    {
+        public DiscriminatorBuilder<T, TDiscriminator> Discriminator<TDiscriminator>(Func<T, TDiscriminator> func)
+        {
+            var discriminatorBuilder = new DiscriminatorBuilder<T, TDiscriminator>();
+            DiscriminatorBuilders.Add(discriminatorBuilder);
+            return discriminatorBuilder;
+        }
+
+        internal override Type Type { get; } = typeof(T);
+    }
+
+    public class DiscriminatorBuilder
+    {
+        internal List<DiscriminatorMapping> Mappings { get; } = new List<DiscriminatorMapping>();
+    }
+
+    public class DiscriminatorBuilder<TBaseType, TDiscriminator> : DiscriminatorBuilder
+    {
+        public DiscriminatorBuilder<TBaseType, TDiscriminator> Mapping<TMappedType>(TDiscriminator discriminatorValue)
+        {
+            Mappings.Add(new DiscriminatorMapping(typeof(TBaseType), typeof(TMappedType), discriminatorValue));
+            return this;
+        }
+
+    }
+
+    internal class DiscriminatorMapping
+    {
+        public Type BaseType { get; }
+        public Type Mappedtype { get; }
+        public object DiscriminatorValue { get; }
+
+        public DiscriminatorMapping(Type baseType, Type mappedtype, object discriminatorValue)
+        {
+            BaseType = baseType;
+            Mappedtype = mappedtype;
+            DiscriminatorValue = discriminatorValue;
+        }
+    }
+}
---
+++ UnitTests/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Cpx86.Swashbuckle.AspNetCore.Discriminator.Extensions;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace UnitTests
+{
+    public class Class1
+    {
+        public void Foo()
+        {
+            var swaggerOptions = new SwaggerGenOptions();
+            swaggerOptions.Polymorphic(p =>
+            {
+                p.Add<BaseType>()
+                    .Discriminator(x => x.Type)
+                    .Mapping<SubTypeA>(Discriminator.A)
+                    .Mapping<SubTypeB>(Discriminator.B);
+            });
+        }
+    }
+
+    internal class BaseType
+    {
+        public Discriminator Type { get; set; }
+    }
+
+    internal enum Discriminator
+    {
+        A,
+        B,
+    }
+
+    internal class SubTypeA : BaseType { }
+    internal class SubTypeB : BaseType { }
+}
